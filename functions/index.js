7ddf94a06f32e7e9ff2a59307b8e5f4ec24f6e16
Hamza Benzaoui <hamzapdd@gmail.com>
2018-12-18 19:30:19 +0100

    add function for when new user sign up

--- functions/index.js
+++ functions/index.js
@@ -27,4 +27,22 @@ exports.projectCreated = functions.firestore
     }
 
     return createNotification(notification);
+});
+
+
+exports.userJoind = functions.auth.user()
+  .onCreate(user =>{
+
+    return admin.firestore().collection('users')
+      .doc(user.uid).get().then(doc =>{
+
+        const newUser = doc.data();
+        const notification = {
+          content: 'Joind the party',
+          user: `${newUser.firstName} ${newUser.lastName}`,
+          time: admin.firestore.FieldValue.serverTimestamp()
+        }
+
+        return createNotification(notification);
+      })
 })

3020890538e16be193c2c53edd2d1d767f5e92b0
Hamza Benzaoui <hamzapdd@gmail.com>
2018-12-18 20:20:19 +0100

    deploy to firebase hosting in https://youplaner.firebaseapp.com/

---
+++ dist/precache-manifest.9151912c394218fb2b181248e3cc4998.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3544f24650a18873ee48",
+    "url": "/static/js/main.3544f246.chunk.js"
+  },
+  {
+    "revision": "f4d2c2a191e26720892a",
+    "url": "/static/js/1.f4d2c2a1.chunk.js"
+  },
+  {
+    "revision": "3544f24650a18873ee48",
+    "url": "/static/css/main.d8dfeefc.chunk.css"
+  },
+  {
+    "revision": "acb88985987b9242536281e4d24c7169",
+    "url": "/index.html"
+  }
+];
